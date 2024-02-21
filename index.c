3fb3a3e7640ec7268413d83aeed753ffbec599e1
Ravid Dayan <dayanravid@gmail.com>
2024-02-21 16:49:49 +0200

    managed to get each token with the line it has been in

--- index.c
+++ index.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 void indexer(/*char *name*/);
-int getMaxLineLength(FILE *file_pointer);
 int getMaxWordLength(FILE *file_pointer);
+int getTokenFromInput(char *des, int tokenLength, int *lineIndex, FILE *file);
+void placeWord(char *token, int index, FILE *file);
 int main(int argc, char *argv[])
 {
+
     indexer();
     /*int i;
     for (i = 0; i < argc; i++)
@@ -17,42 +20,58 @@ int main(int argc, char *argv[])
 }
 void indexer(/*char *fileName*/)
 {
-    FILE *file_pointer;
-    file_pointer = fopen("input.txt", "r");
-    /*printf("%d", getMaxLineLength(file_pointer));*/
-    rewind(file_pointer);
-    printf("%d", getMaxWordLength(file_pointer));
+    char tokens[200][80];
+    char token[80];
+    int lines[200][200];
+    int tokenLength;
+    int lineIndex = 1;
+    int futureLineIndex = 1;
+    int i = 0;
+    int j = 0;
+    FILE *fileInput;
+    /*FILE *fileOutput;*/
+    fileInput = fopen("input.txt", "r");
+    /*fileOutput = fopen("output.txt", "w+");*/
+    tokenLength = getMaxWordLength(fileInput);
+    while (getTokenFromInput(token, tokenLength, &futureLineIndex, fileInput) != 0)
+    {
+        printf("%s ", token);
+        printf("%d\n", lineIndex);
+        lineIndex = futureLineIndex;
+    }
+    printf("%s ", token);
+    printf("%d\n", lineIndex);
+    lineIndex = futureLineIndex;
 }
-int getMaxLineLength(FILE *file)
+
+int getTokenFromInput(char *des, int tokenLength, int *lineIndex, FILE *file)
 {
-    char ch;       /*hold current char*/
-    int start = 0; /*index of the start of the line*/
-    int end = 0;   /*index of the end of the line*/
-    int max = 0;   /*length of longest line*/
-    while ((ch = fgetc(file)) != EOF)
+    int i = 0;
+    char ch;
+    ch = getc(file);
+    while (ch != EOF)
     {
-        if (ch == '\n')
+        if (isspace(ch) != 0)
         {
-            if (max < end - start)
+            if (ch == '\n')
             {
-                max = end - start;
+                (*lineIndex)++;
             }
-            start = end + 1; /*move to start of next line*/
+            return 1;
         }
-        end++;
+        des[i] = ch;
+        des[i + 1] = '\0';
+        i++;
+        ch = getc(file);
     }
-    /*check the last line */
-    if (max < end - start)
-    {
-        max = end - start;
-    }
-    return max;
+    return 0;
 }
 int getMaxWordLength(FILE *file)
 {
     char ch; /*hold current char*/
     int counter = 0;
     int max = 0; /*length of longest line*/
+    rewind(file);
     while ((ch = fgetc(file)) != EOF)
     {
         if (isspace(ch) == 0)
@@ -73,5 +92,6 @@ int getMaxWordLength(FILE *file)
         max = counter;
     }
     counter = 0;
+    rewind(file);
     return max;
 }
