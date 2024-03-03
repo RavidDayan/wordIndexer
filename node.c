d7e2d0d285ae5183e6bbff281f519db8814763f4
Ravid Dayan <dayanravid@gmail.com>
2024-03-03 14:24:04 +0200

    modified node to have a string/int array data

--- node.c
+++ node.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "node.h"
+#include <string.h>
 
-struct node *newNode(void *data)
+struct node *newNode(char *name)
 {
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     if (newNode == NULL)
@@ -10,7 +11,56 @@ struct node *newNode(void *data)
         perror("Error allocating memory");
         exit(EXIT_FAILURE);
     }
-    newNode->data = data;
+    newNode->name = (char *)calloc(strlen(name) / sizeof(char), sizeof(char));
+    newNode->lines = (int *)calloc(LINE_BUFFER, sizeof(int));
+    newNode->lines[LINE_BUFFER - 1] = LINE_SENTRY;
     newNode->next = NULL;
     return newNode;
+}
+void setNext(struct node *this, struct node *next)
+{
+    this->next = next;
+}
+void setLine(struct node *this, int line)
+{
+    int i;
+    for (i = 0; this->lines[i] != LINE_SENTRY; i++)
+    {
+        if (this->lines[i] == 0)
+        {
+            this->lines[i] = line;
+            return;
+        }
+    }
+    if (this->lines[i] == LINE_SENTRY)
+    {
+        this->lines = realloc(this->lines, (strlen(this->lines) / sizeof(int)) + LINE_BUFFER);
+        if (this->lines == NULL)
+        {
+            perror("Error reallocating memory");
+        }
+        else
+        {
+            this->lines[(strlen(this->lines) / sizeof(int)) - 1] = LINE_SENTRY;
+            this->lines[i] = line;
+        }
+    }
+}
+struct node *getNext(struct node *this)
+{
+    return this->next;
+}
+char *getName(struct node *this)
+{
+    return this->name;
+}
+int *getLines(struct node *this)
+{
+    return this->lines;
+}
+void freeNode(struct node *this)
+{
+    free(this->name);
+    free(this->lines);
+    free(this);
 }
--- node.h
+++ node.h
@@ -1,6 +1,15 @@
+#define LINE_BUFFER 5
+#define LINE_SENTRY -1
 struct node
 {
-    void *data;
+    char *name;
+    int *lines;
     struct node *next;
 };
-struct node *newNode(void *data);
+struct node *newNode(char *name);
+void setNext(struct node *this, struct node *next);
+void setLine(struct node *this, int line);
+struct node *getNext(struct node *this);
+char *getName(struct node *this);
+int *getLines(struct node *this);
+void freeNode(struct node *this);
